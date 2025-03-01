e30706b79da69127f2ffa64b62afe21337b920d0
Rahul Makwana <rahulm.octal8@gmail.com>
2025-03-01 15:07:07 +0530

    fix

--- src/components/Demo.js
+++ src/components/Demo.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Demo3 from './Demo3'
+import Demo1 from './Demo1';
 
 function demo() {
 
@@ -10,6 +11,8 @@ function demo() {
     <>
     <div>demo</div>
     <Demo3 changeCount = {countList} />
+    <div>demo3</div>
+    <Demo1 changeCount = {countList} />
     </>
   )
 }
